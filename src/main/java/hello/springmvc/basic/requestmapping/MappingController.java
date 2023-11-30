a8b2d4e90e1f7360b82949103589cfdc0d22a80f
3zzinup <wintiger98@gmail.com>
2023-11-30 15:31:45 +0900

    springmvc basic - request mapping

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,75 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MappingController {
+  @GetMapping("/hello-basic")
+  public String helloBasic() {
+    log.info("helloBasic");
+    return "ok";
+  }
+
+  @GetMapping("/mapping-get-v2")
+  public String mappingGetV2() {
+    log.info("mapping get v2");
+    return "ok";
+  }
+
+  @GetMapping("/mapping/{userId}")
+  public String mappingPath(@PathVariable("userId") String data) {
+    log.info("mappingPath userId={}", data);
+    return "ok";
+  }
+
+  @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+  public String mappingPath(@PathVariable String userId, @PathVariable Long orderId){
+    log.info("mappingPath userId={} orderId={}", userId, orderId);
+    return "ok";
+  }
+
+  @GetMapping(value = "mapping-param", params = "mode=debug")
+  public String mappingParam() {
+    log.info("mappingParam");
+    return "ok";
+  }
+
+  @GetMapping(value = "/mapping-header", headers = "mode=debug")
+  public String mappingHeader() {
+    log.info("mappingHeader");
+    return "ok";
+  }
+
+  /**
+   * Content-Type 헤더 기반 추가 매핑 Media Type
+   * consumes="application/json"
+   * consumes="!application/json"
+   * consumes="application/*"
+   * consumes="*\/*"
+   * MediaType.APPLICATION_JSON_VALUE
+   */
+  @PostMapping(value = "/mapping-consume", consumes = "application/json")
+  public String mappingConsumes() {
+    log.info("mappingConsumes");
+    return "ok";
+  }
+
+  /**
+   * Accept 헤더 기반 Media Type
+   * produces = "text/html"
+   * produces = "!text/html"
+   * produces = "text/*"
+   * produces = "*\/*"
+   */
+  @PostMapping(value = "/mapping-produce", produces = "text/html")
+  public String mappingProduces() {
+    log.info("mappingProduces");
+    return "ok";
+  }
+}
